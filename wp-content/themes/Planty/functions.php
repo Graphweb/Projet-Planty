12d73d72dbdb86c8fd09fa1dbbdfb8ef000fca10
PARIEL Dominique <dominiquepariel88@gmail.com>
2024-07-24 15:20:33 +0400

    création des pages + formulaire + hook

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -5,5 +5,14 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
+//  lien pour lier le css enfant//
+ wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
 }
+// code pour ciblé avec un hook//
+function ajouter_lien_admin($items, $args) {
+    if (is_user_logged_in() && $args->theme_location == 'primary') {
+        $items .= '<li class="menu-item"><a href="' . admin_url() . '">Admin</a></li>';
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'ajouter_lien_admin', 10, 2);
